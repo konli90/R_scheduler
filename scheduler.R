8e204787811d36a080141eebc731fb2e326e5439
konli90 <kon.lipenga@gmail.com>
2023-05-24 12:27:59 +0200

    added new rscript

--- scheduler.R
+++ scheduler.R
@@ -1,7 +1,7 @@
 library(taskscheduleR)
 
 task <- taskscheduler_create(taskname = "VL-Weekly-R-Script-Task",
-                             rscript = "C:\\Users\\MARYLAND\\Documents\\scheduler\\vl_weekly.R",
+                             rscript = "C:\\Users\\Konli\\Documents\\scheduler\\vl_weekly.R",
                              schedule = "DAILY",
                              startdate = format(Sys.Date(), "%m/%d/%Y"),
                              starttime = "11:51",
